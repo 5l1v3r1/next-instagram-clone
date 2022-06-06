51e575f1de1da03f2ac2a92018fa912f8c5ac328
Ozgenur Ozkara <ozgenurozkara@gmail.com>
2022-06-06 15:07:16 +0300

    added search bar

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
+import { SearchIcon } from '@heroicons/react/outline';
 
 export default function  () {
   return (
-    <div> 
-        {/* Left */}
+   
+        
         <div className='flex items-center justify-between max-w-7xl'>
+            {/* Left */}
             <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
                 <Image
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png" 
@@ -20,11 +22,19 @@ export default function  () {
                 className='object-contain'
                 />
             </div>
+            {/* Middle */}
+            <div className='relative mt-1'>
+                <div className='absolute top-2 left-2'>
+                    <SearchIcon className='h-5 text-gray-500'/>
+                </div>
+                <input type="text" placeholder= "Search" className='bg-gray-50 pl-11 border-gray-500 text-sm focus:ring-black focus:border-black rounded' />
+            </div>
+             {/* Right */}
             <h1>sağ taraf</h1>
         </div>
         
-        {/* Middle */}
-        {/* Right */}
-    </div>
+        
+       
+   
   )
 }
