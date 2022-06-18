21a4b8407be21e76e3418eb6931b7d4de523f5dd
Ozgenur Ozkara <ozgenurozkara@gmail.com>
2022-06-18 14:36:25 +0300

    edited mini profile and story components to include session

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,12 +1,16 @@
-
+import { UserAddIcon } from "@heroicons/react/outline";
+import { useSession, signOut } from "next-auth/react";
 export default function MiniProfile() {
+  const {data: session} =useSession();
   return (
   <div className="flex items-center justify-between ml-10 mt-14">
-    <img className="h-16 rounded-full border p-[2px]" src="https://pbs.twimg.com/profile_images/1416435456468729865/G578oM3C_400x400.jpg" alt="user-image" />
+    <img className="h-16 rounded-full border p-[2px]"
+     src={session?.user.image}
+      alt="user-image" />
     <div className="flex-1 ml-4">
-        <h2 className="font-bold">ozgeozkaraa01</h2>
+        <h2 className="font-bold">{session?.user.username}</h2>
     </div>
-    <button className="font-semibold text-sm text-blue-500 ">Çıkış Yap</button>
+    <button onClick={signOut} className="font-semibold text-sm text-blue-500 ">Çıkış Yap</button>
 
 
   </div>
